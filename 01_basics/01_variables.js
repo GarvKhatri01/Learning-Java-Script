6eb868187d6e4ba1925b47639791451f2472d1c0
GarvKhatri01 <krishnakhatri1126@gmail.com>
2026-08-09 12:51:22 +0530

    learned variabes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 14423
+let accountEmail ="garv123@gmail.com"
+var accountPass = "12345"
+
+accountCity = "jaipur"
+let accountState;
+// accountId = 2;  //Not allowed
+accountEmail = "hello"
+accountPass ="2346792"
+accountCity = "bhopal"
+/* 
+    Prefer not to use var 
+    bcoz of issue in block scope or functional scope
+*/
+
+// console.log(accountId);
+// console.log(accountEmail);
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
