458df76ff5e2828cf8a7ab2071514220d1cb1ff1
GarvKhatri01 <krishnakhatri1126@gmail.com>
2026-08-09 15:41:49 +0530

    conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,35 @@
+
+// let score = "33ab"  convert to number ..but the number contains ..NaN (Not a number)
+// let score = "33" // will work
+// let score = null  // will convert into 0
+// let score = undefined  // Nan conversion will happen
+
+// let score =true  // 1 or 0 
+
+// console.log(typeof(score));
+// let value = Number(score)
+
+// console.log(typeof (value))
+// console.log(value)
+
+// let isLogged = 1;
+
+// let boolLogged = Boolean(isLogged)  // 0 => false .. else true always
+
+// ""  => false else "hitesh" => true
+
+// console.log(typeof(boolLogged));
+// console.log(boolLogged);
+
+let number = 34
+
+let stringNum = String(number)
+
+console.log(typeof(stringNum));
+console.log(stringNum);
+
+
+
+
+
+
