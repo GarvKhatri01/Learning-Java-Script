a6a6c6c860068b06203c8baac971069c748c087b
GarvKhatri01 <krishnakhatri1126@gmail.com>
2026-08-09 15:14:29 +0530

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // Treat all js code as newer version
+
+// alert(3+ 3) // we are using node.js not the browser 
+
+let name = "Garv"
+let age = 19
+let isLogged = true
+let state;  //undefinded
+
+let value = null  // not undefined its null 
+//Premitive data types
+
+//number => 2^53 something
+//bigint
+//string
+//boolean =>true/false
+
+//null - it is a value: standalone value               
+//undefined
+
+//symbol  =>unique
+
+//object 
+
+console.log(typeof age);
+console.log(typeof undefined)  // undefinded type
+console.log(typeof null)  // object type
+
